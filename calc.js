08cc0ffbe4505662085c93dfc620cac77e79b2c6
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-04-05 15:10:08 -0400

    Add dom styling for header and container elements

--- calc.js
+++ calc.js
@@ -1,5 +1,20 @@
 /* calc.js */
 
+// creating and styling header
+const headerText = document.createElement("h2");
+headerText.classList.add("headerText");
+headerText.textContent = "Mathematical!";
+headerText.style.textAlign = "center";
+header.appendChild(headerText);
+
+// creating and styling container element
+const container = document.getElementById("container");
+container.classList.add("container");
+document.getElementById("container").style.width = "300px";
+document.getElementById("container").style.height = "500px";
+container.style.border = "solid";
+container.style.borderWidth = "thin";
+
 // operation variables
 // with temporary values for testing
 let userNum1 = 5;
