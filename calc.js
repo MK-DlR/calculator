2238b4f7dbe3fec4d5678fa51dbfdf04ef35a20f
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-04-05 13:01:01 -0400

    Create basic mathematical operators and temporary number variables for testing

--- calc.js
+++ calc.js
@@ -1,1 +1,43 @@
 /* calc.js */
+
+// temporary number variables
+let a = 8;
+let b = 2;
+
+// math functions
+// addition
+function add(a, b) {
+  console.log(a + b);
+  return a + b;
+}
+
+// subtraction
+function subtract(a, b) {
+  console.log(a - b);
+  return a - b;
+}
+
+// multiplication
+function multiply(a, b) {
+  console.log(a * b);
+  return a * b;
+}
+
+// division
+function divide(a, b) {
+  console.log(a / b);
+  return a / b;
+}
+
+// modulo
+function modulo(a, b) {
+  console.log(a % b);
+  return a % b;
+}
+
+// calling functions to test
+add(a, b);
+subtract(a, b);
+multiply(a, b);
+divide(a, b);
+modulo(a, b);
